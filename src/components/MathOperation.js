399aba903344237699f189ae9c082a74eb49bac7
jmendozacr <mendoza077@gmail.com>
2021-01-27 15:01:51 -0600

    update

---
+++ src/components/MathOperation.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Button from "../components/Button";
+import PropTypes from "prop-types";
+
+const MathOperation = ({onClickOperation, onClickEqual}) => (
+  <section className="math-operations">
+        <Button text={"+"} clickHandler={onClickOperation}/>
+        <Button text={"-"} clickHandler={onClickOperation}/>
+        <Button text={"*"} clickHandler={onClickOperation}/>
+        <Button text={"/"} clickHandler={onClickOperation}/>
+        <Button text={"="} clickHandler={onClickEqual}/>
+  </section>
+);
+
+MathOperation.propTypes = {
+  onClickOperation: PropTypes.func.isRequired,
+  onClickEqual: PropTypes.func.isRequired
+}
+  
+export default MathOperation;
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,14 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Result = ({ value }) => {
-
-  return (
-    <div className="result">
-      {value}
-    </div>
-  );
-}
+const Result = ({ value }) => (
+  <div className="result">
+    {value}
+  </div>
+);
 
 Result.propTypes = {
   value: PropTypes.number.isRequired
