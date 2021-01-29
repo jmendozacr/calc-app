aca003338d059ae724a962df20a175463139638f
jmendozacr <mendoza077@gmail.com>
2021-01-28 20:13:48 -0600

    render button using map function

--- src/components/Numbers.js
+++ src/components/Numbers.js
@@ -3,18 +3,19 @@ import Button from "./Button/Button";
 import PropTypes from "prop-types";
 import "./Button/Button.css"
 
-const Numbers = ({onClickNumber}) => (
+const textNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const renderButtons = onClickNumber => {
+  return textNumber.map((item, index) => {
+    return <Button text={item} key={index} clickHandler={onClickNumber}/>
+  })
+}
+
+const Numbers = ({ onClickNumber }) => (
   <section className="numbers">
-    <Button text={"1"} clickHandler={onClickNumber}/>
-    <Button text={"2"} clickHandler={onClickNumber}/>
-    <Button text={"3"} clickHandler={onClickNumber}/>
-    <Button text={"4"} clickHandler={onClickNumber}/>
-    <Button text={"5"} clickHandler={onClickNumber}/>
-    <Button text={"6"} clickHandler={onClickNumber}/>
-    <Button text={"7"} clickHandler={onClickNumber}/>
-    <Button text={"8"} clickHandler={onClickNumber}/>
-    <Button text={"9"} clickHandler={onClickNumber}/>
-    <Button text={"0"} clickHandler={onClickNumber}/>
+    {
+      renderButtons(onClickNumber)
+    }
+
   </section>
 );
 
